494665e466b56955bbf260f5d696b6cc6a07af44
nsragusa <nicolas.ragusa@tufts.edu>
2019-04-23 15:11:04 -0400

    Update microbit_temperature.py

--- microbit_temperature.py
+++ microbit_temperature.py
@@ -10,12 +10,14 @@ while True:
     if microbit.button_a.was_pressed() == True:                         # waits for button_a pressed
         microbit.sleep(3000)                                            # 3 second delay
         time0 = microbit.running_time()
-        with open('temperature_data' + a + '.txt', 'w') as my_file:           # makes file named 1 to 100
-            for i in range(3000):
+        with open('newtemperature_data' + a + '.txt', 'w') as my_file:           # makes file named 1 to 100
+            for i in range(1000):
                 microbit.display.set_pixel(2, 2, 9)                     # indicator that it is recording data
-                microbit.sleep(5)                                       # sleep at 25 milliseconds
+                microbit.sleep(25)                                       # sleep at 25 milliseconds
                 y = str(microbit.temperature())                         # writes data in file
                 time1 = microbit.running_time()
-                elapsed_time = str(time1-time0)                         # gets the elapsed time from the last data write
-                my_file.write(elapsed_time + ', ' + y + '\n')           # writes data, makes new lines
-    microbit.display.set_pixel(2, 2, 0)
+                elapsed_time = str((time1-time0)/1000)                         # gets the elapsed time from the last data write
+                my_file.write(elapsed_time + ', ' + y + '\n')
+            break
+                # writes data, makes new lines
+microbit.display.set_pixel(2, 2, 0)
