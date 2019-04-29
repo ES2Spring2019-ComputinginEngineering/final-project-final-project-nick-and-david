fc6755d93be079b2c1f8ed8e4e87230a857af7c9
nsragusa <nicolas.ragusa@tufts.edu>
2019-04-29 00:29:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -32,21 +32,21 @@ if order == '1':
         order = 1.2
 
 if order == 1.1:
-    Function = functionGrowth(Time, K, T)
+    #Function = functionGrowth(Time, K, T)
     n = 24
-    popt, pcov = curve_fit(Function, Time, Output,bounds=(n,1000))
-    s1.graphDataTF(Time, Input, output_filt, Time_axis_name, Out_axis_name)
+    popt, pcov = curve_fit(s1.functionGrowth, Time, Output,bounds=(n,1000))
+    s1.graphDataTF(Time, Input, output_filt, Time_axis_name, Out_axis_name, popt)
     print('The step response is:',popt[0],'(1-e^(-t/'+str(popt[1])+')')
 elif order == 1.2:
     s1.functiondecay(Time, K, T)
-    popt, pcov = curve_fit(functiongrowth, Time, Output)
+    popt, pcov = curve_fit(s1.functionDecay, Time, Output)
     s1.graphDataTF(Time, Input, output_filt, Time_axis_name, Out_axis_name)
     print('The step response is:',popt[0],'(1-e^(-t/'+str(popt[1])+')')
 elif order == '2':
-    Zeta, Phi, K_dc, Period, Omega_d, Y_t, Optimize_OG = s2.EstimateSecondOrderCurve
+    Zeta, Phi, K_dc, Period, Omega_d, Y_t, Optimize_OG = s2.EstimateSecondOrderCurve(Output, Input, Time)
     n=1
     s2.graphDataTF(Time, Input, Output, Time_axis_name, Out_axis_name, Period, Y_t,n)
     optimize = input('Would you like to optimize this curve? 1=yes 2=no: ')
     if optimize == '1':
-        Y_t, n = OptimizeCurve(Output, Period, K_dc, Input, Zeta, Omega_d, Time, Phi, Optimize_OG)
+        Y_t, n = s2.OptimizeCurve(Output, Period, K_dc, Input, Zeta, Omega_d, Time, Phi, Optimize_OG)
         s2.graphDataTF(Time, Input, Output, Time_axis_name, Out_axis_name, Period, Y_t,n)
