5960c43d0713ed67f29a4c514f1455fcf4a2f5fb
nsragusa <nicolas.ragusa@tufts.edu>
2019-05-02 19:28:11 -0400

    Update SecondOrderFunctions.py

--- SecondOrderFunctions.py
+++ SecondOrderFunctions.py
@@ -38,19 +38,19 @@ def graphDataTF(Time, Input, Output, Time_axis_name, Out_axis_name, Period, Y_t,
     plt.show()
 
 
-def Filter(Output, Time, Input, Time_axis_name, Out_axis_name):                                        #
+def Filter(Output, Time, Input, Time_axis_name, Out_axis_name):                                        #filters the data to the requested kernel size
     to_filter = np.copy(Output)
     Kernel = int(input('Please enter the kernel size you would like (odd integers only): '))
     RPM_filt = sig.medfilt(to_filter,kernel_size=Kernel)
     s1.graphData(Time, Input, RPM_filt, Time_axis_name, Out_axis_name)
     message = "If you would like to try a different kernel size, press '1'. If not, press '2': "
     again = int(s1.yesOrNo(message))
-    if again == 1:
+    if again == 1:                                                                                      #allows the user to try a different kernel size
         Filter(Output, Time, Input, Time_axis_name, Out_axis_name)
     return RPM_filt
 
 
-def EstimateSecondOrderCurve(Output,Input,Time):
+def EstimateSecondOrderCurve(Output,Input,Time):                                                         #estimates the best fit for the curve based on manually calculated variables
     SteadyState, _ = stats.mode(Output)
     OSR = (np.max(Output)-SteadyState)/SteadyState
     Zeta = -float(np.log(OSR)/np.sqrt(np.pi**2 + np.log(OSR)**2))
@@ -63,18 +63,18 @@ def EstimateSecondOrderCurve(Output,Input,Time):
     Y_t = K_dc*Input[1:]*(1-(1/(np.sqrt(1-Zeta**2)))*np.exp(-Zeta*Omega_d*Time[1:])*np.cos(Omega_d*Time[1:]-Phi))
     Optimize_OG = np.sum(np.sqrt((Y_t - Output[1:])**2))
     Correlation = np.sum((Output[1:]-np.mean(Output[1:]))*(Y_t-np.mean(Y_t)))/\
-                  (np.sqrt(np.sum(((Output[1:]-np.mean(Output[1:]))**2))*np.sum((Y_t-np.mean(Y_t))**2)))
+                  (np.sqrt(np.sum(((Output[1:]-np.mean(Output[1:]))**2))*np.sum((Y_t-np.mean(Y_t))**2)))    #calculates how well the data matches up with the curve found
     print('Correlation is:',Correlation)
     return Zeta, Phi, K_dc, Period, Omega_d, Y_t, Optimize_OG, n, SteadyState
 
 
-def OptimizeCurve(Output, Period, K_dc, Input, Zeta, Omega_d, Time, Phi, Optimize_OG, n, SteadyState):
+def OptimizeCurve(Output, Period, K_dc, Input, Zeta, Omega_d, Time, Phi, Optimize_OG, n, SteadyState):      #recalculates omega_d to come up with the best period for the data
     for i in range(int(len(Output)/2)):
         Delta_Period_Opt = Time[Period[1]+i]-Time[Period[0]]
         Omega_d_Opt = (2*np.pi)/Delta_Period_Opt
         Y_t_Opt = K_dc*Input[1:]*(1-(1/(np.sqrt(1-Zeta**2)))*np.exp(-Zeta*Omega_d_Opt*Time[1:])*np.cos(Omega_d_Opt*Time[1:]-Phi))
         Optimize = np.sum(np.sqrt((Y_t_Opt - Output[1:])**2))
-        if Optimize < Optimize_OG:
+        if Optimize < Optimize_OG:                                                                          #checks if this optimization is better than the last, saving the best
             num = i
             Y_t = Y_t_Opt
             Optimize_OG = Optimize
